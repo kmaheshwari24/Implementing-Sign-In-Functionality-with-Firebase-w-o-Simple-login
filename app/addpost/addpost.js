3e4866fc23c99b5a376ac290dc356842e49baffc
kmaheshwari24 <khushboo24@gmail.com>
2016-03-29 00:49:56 +0530

    Add Publish Post functionality

--- app/addpost/addpost.js
+++ app/addpost/addpost.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.addpost',['ngRoute'])
+angular.module('myApp.addpost',['ngRoute', 'firebase'])
 
 //Declare route
 .config(['$routeProvider', function($routeProvider){
@@ -13,5 +13,27 @@ angular.module('myApp.addpost',['ngRoute'])
 // Register Controller
 
 .controller('AddPostCtrl', ['$scope', function($scope){
+
+		// add post logic will be here
+
+	$scope.AddPost = function(){
+		var ref = new Firebase("https://blazing-heat-8641.firebaseio.com");
+		var postRef = ref.child("posts");
+
+		var title = $scope.article.title;
+		var post = $scope.article.post;
+
+		postRef.push({
+			title: title, 
+			post: post
+		},function(error){
+			if (error) {
+				 console.log("Data could not be saved.", error);
+  			} else {
+    			console.log("Data saved successfully.", postRef);
+    		}
+		});
+
+	};
 	
 }]);
