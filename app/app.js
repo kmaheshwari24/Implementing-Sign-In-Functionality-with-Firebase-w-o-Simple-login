757105510fb61de325a8921a73a9fa596dcc86f2
kmaheshwari24 <khushboo24@gmail.com>
2016-03-28 15:42:47 +0530

    Add validation to SignIn and Registration pages

--- app/app.js
+++ app/app.js
@@ -1,5 +1,7 @@
+'use strict';
+
 angular.module('myApp', [
-  'ngRoute', 'myApp.home'
+  'ngRoute', 'myApp.home', 'myApp.register'
 ]).
 config(['$routeProvider', function($routeProvider) {
   // routes will be here
---
+++ app/register/register.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('myApp.register',['ngRoute'])
+
+//Declare route
+.config(['$routeProvider', function($routeProvider){
+	$routeProvider.when('/register', {
+		templateUrl: 'register/register.html',
+		controller: 'RegisterCtrl'
+	});
+}])
+
+// Register Controller
+
+.controller('RegisterCtrl', ['$scope', function($scope){
+	
+}]);
