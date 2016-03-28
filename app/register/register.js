d8c8db191b3b723241a6d5ca42207f060f655b98
kmaheshwari24 <khushboo24@gmail.com>
2016-03-28 18:07:16 +0530

    Redirection to Home from Register page

--- app/register/register.js
+++ app/register/register.js
@@ -12,33 +12,31 @@ angular.module('myApp.register',['ngRoute', 'firebase'])
 
 // Register Controller
 
-.controller('RegisterCtrl', ['$scope', '$location', function($scope, $location){
-		
-	$scope.signUp = function(event){
-		event.preventDefault();
-		if(!$scope.regForm.$invalid){
-			console.log("Valid for submission");
-		}
+.controller('RegisterCtrl', ['$scope', '$location', '$firebaseAuth', function($scope, $location, $firebaseAuth){
+	
+	var firebaseObj = new Firebase("https://blazing-heat-8641.firebaseio.com");
+	var auth = $firebaseAuth(firebaseObj);
 
-		var username = $scope.user.email;
+	$scope.signUp = function(){
+		
+		if(!$scope.regForm.$invalid){
+
+		var email = $scope.user.email;
  		var password = $scope.user.password;	
 
-		var ref = new Firebase("https://blazing-heat-8641.firebaseio.com");
-		ref.createUser({
-  			email    : username,
-            password : password
-			}, function(error, userData) {
-  				if (error) {
-    				console.log("Error creating user:", error);
-    				$scope.regError = true;
-    				$scope.regErrorMessage = error.message;
-  				} else {
-    				console.log("Successfully created user account with uid:", userData.uid);
-    				$location.path('/home');
-  			}
-		});
+		//	if (email && password) {
+				auth.$createUser({email, password})
+					.then(function() {
+  						console.log("Successfully created user account");
+    					$location.path('/home');
+    				}, function(error){
+    					console.log("Error creating user:", error);
+    					$scope.regError = true;
+    					$scope.regErrorMessage = error.message;
+  					});		
+  		//	}
+  		}
 
 	};
-
 
 }]);
