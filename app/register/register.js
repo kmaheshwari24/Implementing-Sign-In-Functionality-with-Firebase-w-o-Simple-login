8d16a037def7f2cda17c026a70ec020a07ec26f6
kmaheshwari24 <khushboo24@gmail.com>
2016-03-28 16:36:22 +0530

    Add User Creation with Firebase

--- app/register/register.js
+++ app/register/register.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.register',['ngRoute'])
+angular.module('myApp.register',['ngRoute', 'firebase'])
 
 //Declare route
 .config(['$routeProvider', function($routeProvider){
@@ -13,5 +13,28 @@ angular.module('myApp.register',['ngRoute'])
 // Register Controller
 
 .controller('RegisterCtrl', ['$scope', function($scope){
-	
+		
+	$scope.signUp = function(){
+		if(!$scope.regForm.$invalid){
+			console.log("Valid for submission");
+		}
+
+		var username = $scope.user.email;
+ 		var password = $scope.user.password;	
+
+		var ref = new Firebase("https://blazing-heat-8641.firebaseio.com");
+		ref.createUser({
+  			email    : username,
+            password : password
+			}, function(error, userData) {
+  				if (error) {
+    				console.log("Error creating user:", error);
+  				} else {
+    				console.log("Successfully created user account with uid:", userData.uid);
+  			}
+		});
+
+	};
+
+
 }]);
