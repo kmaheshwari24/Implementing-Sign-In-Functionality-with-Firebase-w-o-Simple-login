317705fed590e8990506574c8591a39d551c884e
kmaheshwari24 <khushboo24@gmail.com>
2016-03-29 13:10:52 +0530

    Include Edit and Delete Buttons

--- app/welcome/welcome.js
+++ app/welcome/welcome.js
@@ -11,12 +11,18 @@ angular.module('myApp.welcome', ['ngRoute', 'firebase'])
 }])
 
 // Home controller
-.controller('WelcomeCtrl', ['$scope', 'CommonProp', '$firebaseArray', function($scope, CommonProp, $firebaseArray) {
+.controller('WelcomeCtrl', ['$scope', 'CommonProp', '$firebaseArray', '$firebaseObject', function($scope, CommonProp, $firebaseArray, $firebaseObject) {
 
 $scope.username = CommonProp.getUser();
 
 var sync = new Firebase("https://blazing-heat-8641.firebaseio.com/Articles/posts");
 $scope.articles = $firebaseArray(sync);
+
+$scope.editPost = function(id) {
+	var editContent = new Firebase("https://blazing-heat-8641.firebaseio.com/Articles/posts/" + id);
+	$scope.postToUpdate = $firebaseObject(editContent);
+	$('#editModal').modal(); //trigger the modal pop up
+}
 
 
 }]);
