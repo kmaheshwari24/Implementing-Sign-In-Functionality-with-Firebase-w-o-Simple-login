26258763aa310005dbcaab6e33c59d797b0f7c0c
kmaheshwari24 <khushboo24@gmail.com>
2016-03-29 14:00:08 +0530

    Update Firebase Data on Post Update

--- app/welcome/welcome.js
+++ app/welcome/welcome.js
@@ -18,11 +18,34 @@ $scope.username = CommonProp.getUser();
 var sync = new Firebase("https://blazing-heat-8641.firebaseio.com/Articles/posts");
 $scope.articles = $firebaseArray(sync);
 
+// Passing title and post to the edit modal for editing a post
+
 $scope.editPost = function(id) {
 	var editContent = new Firebase("https://blazing-heat-8641.firebaseio.com/Articles/posts/" + id);
 	$scope.postToUpdate = $firebaseObject(editContent);
 	$('#editModal').modal(); //trigger the modal pop up
-}
+};
+
+// Update function to reset data in firebase
+
+$scope.update = function(){
+
+	//console.log($scope.postToUpdate.$id);
+	var fb = new Firebase("https://blazing-heat-8641.firebaseio.com/Articles/posts");
+	var UpdatedPost = fb.child($scope.postToUpdate.$id);
+	UpdatedPost.update({
+		title: $scope.postToUpdate.title,
+		post: $scope.postToUpdate.post,
+		emailId: $scope.postToUpdate.emailId},
+		function(error){
+			if (error) {
+				console.log("Error: ",error);
+			} else {
+				$('#editModal').modal('hide');
+				//console.log("Update Successful");
+			}
+		});
+};
 
 
 }]);
